c4aa5d7878cefaf8b3e15b7d3c8fedf0343b36b9
Roger Bonati <roger.bonati@web.de>
2024-04-19 01:38:48 +0200

    added prettier for yaml

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -9,6 +9,7 @@ return {
       },
       formatters_by_ft = {
         lua = { 'stylua' },
+        yaml = { 'prettier' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
