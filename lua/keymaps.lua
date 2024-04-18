b866ab92f931bfc98b8c8091d83ea425eb86ab32
Roger Bonati <roger.bonati@1und1.de>
2024-04-18 14:29:21 +0200

    linting added

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -20,10 +20,10 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 -- TIP: Disable arrow keys in normal mode
--- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
--- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
@@ -33,6 +33,11 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- linting
+vim.keymap.set('n', '<leader>l', function()
+  require('conform').format { bufnr = vim.api.nvim_get_current_buf() }
+end, { desc = 'Trigger formatting for current file' })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
@@ -45,6 +50,24 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
   callback = function()
     vim.highlight.on_yank()
+  end,
+})
+
+-- linting
+--vim.api.nvim_create_autocmd('BufWritePre', {
+--  pattern = '*.yaml,*.yml',
+--  callback = function(args)
+--    require('conform').format { bufnr = args.buf }
+--  end,
+--})
+local group = vim.api.nvim_create_augroup("Format", { clear = true })
+
+
+vim.api.nvim_create_autocmd('BufWritePre', {
+  group = group,
+  pattern = '*.yaml,*.yml',
+  callback = function()
+    vim.cmd '%s/\\s\\+$//e'
   end,
 })
 
