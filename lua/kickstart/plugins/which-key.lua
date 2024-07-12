17c5c707decb64dbfde4884a3185bd22be4da384
Roger Bonati <roger.bonati@web.de>
2024-07-13 00:42:57 +0200

    fixed deprecated which-key config

--- lua/kickstart/plugins/which-key.lua
+++ lua/kickstart/plugins/which-key.lua
@@ -18,22 +18,31 @@ return {
     'folke/which-key.nvim',
     event = 'VimEnter', -- Sets the loading event to 'VimEnter'
     config = function() -- This is the function that runs, AFTER loading
-      require('which-key').setup()
+      require('which-key').setup {
+        -- Registering key mappings
+        mappings = {
+          -- Defining key mappings according to the new spec format
+          { '<leader>c', group = '[C]ode' },
+          { '<leader>c_', hidden = true }, -- Hidden mapping for <leader>c_
+          { '<leader>d', group = '[D]ocument' },
+          { '<leader>d_', hidden = true }, -- Hidden mapping for <leader>d_
+          { '<leader>h', group = 'Git [H]unk' },
+          { '<leader>h_', hidden = true }, -- Hidden mapping for <leader>h_
+          { '<leader>r', group = '[R]ename' },
+          { '<leader>r_', hidden = true }, -- Hidden mapping for <leader>r_
+          { '<leader>s', group = '[S]earch' },
+          { '<leader>s_', hidden = true }, -- Hidden mapping for <leader>s_
+          { '<leader>t', group = '[T]oggle' },
+          { '<leader>t_', hidden = true }, -- Hidden mapping for <leader>t_
+          { '<leader>w', group = '[W]orkspace' },
+          { '<leader>w_', hidden = true }, -- Hidden mapping for <leader>w_
 
-      -- Document existing key chains
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-        ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
+          -- visual mode
+          { '<leader>h', desc = 'Git [H]unk', mode = 'v' },
+        },
+
+        -- Additional setup options (if needed)
       }
-      -- visual mode
-      require('which-key').register({
-        ['<leader>h'] = { 'Git [H]unk' },
-      }, { mode = 'v' })
     end,
   },
 }
