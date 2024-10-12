6f5b5c27cdbcee0e1c21c240ced6c2bdfc6ef1dd
Roger Bonati <roger.bonati@web.de>
2024-10-12 18:25:36 +0200

    added notification for macro recording

--- lua/custom/plugins/nvim-noice.lua
+++ lua/custom/plugins/nvim-noice.lua
@@ -3,6 +3,13 @@ return {
   'folke/noice.nvim',
   event = 'VeryLazy',
   opts = {
+
+    routes = {
+      {
+        view = 'notify',
+        filter = { event = 'msg_showmode' },
+      },
+    },
     -- add any options here
   },
   dependencies = {
