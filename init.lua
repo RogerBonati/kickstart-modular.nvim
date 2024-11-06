d48b4b2abd30b54679460b38119508fba122a9c2
Roger Bonati <roger.bonati@web.de>
2024-11-06 19:41:42 +0100

    Revert "added helm filetype"
    This reverts commit 38d4202c5a5b311ddb34bf6cf85b512efb4eb26f.

--- init.lua
+++ init.lua
@@ -105,13 +105,5 @@ require 'lazy-bootstrap'
 -- [[ Configure and install plugins ]]
 require 'lazy-plugins'
 
--- Filetype detection
-
-vim.filetype.add {
-  pattern = {
-    ['.*/templates/.*%.yaml'] = 'helm',
-  },
-}
-
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
