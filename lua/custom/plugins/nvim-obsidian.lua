bcdb5b38a83e0605f6ac38a1854ca137bc52980c
Roger Bonati <roger.bonati@web.de>
2024-11-14 17:36:32 +0100

    datumsnotizen funktionieren jetzt

--- lua/custom/plugins/nvim-obsidian.lua
+++ lua/custom/plugins/nvim-obsidian.lua
@@ -20,7 +20,18 @@ return {
       },
       ui = { enable = false },
       notes_subdir = '_Datumsnotizen',
-
+      daily_notes = {
+        -- Optional, if you keep daily notes in a separate directory.
+        folder = '_Datumsnotizen',
+        -- Optional, if you want to change the date format for the ID of daily notes.
+        -- date_format = "%Y-%m-%d",
+        -- Optional, if you want to change the date format of the default alias of daily notes.
+        alias_format = '%B %-d, %Y',
+        -- Optional, default tags to add to each new daily note created.
+        default_tags = { 'daily-notes' },
+        -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+        template = '_Vorlagen/Datumsnotizen',
+      },
       -- Optional, for templates (see below).
       templates = {
         folder = '_Vorlagen',
