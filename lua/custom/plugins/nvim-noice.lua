5e2694a32f9700b9e81dade834700c93923d8b07
Roger Bonati <roger.bonati@web.de>
2025-03-18 18:48:05 +0100

    get rid of notify

--- lua/custom/plugins/nvim-noice.lua
+++ lua/custom/plugins/nvim-noice.lua
@@ -21,9 +21,9 @@ return {
     --   `nvim-notify` is only needed, if you want to use the notification view.
     --   If not available, we use `mini` as the fallback
     -- { 'rcarriga/nvim-notify', enabled = false },
-    { 'rcarriga/nvim-notify', enabled = true, opts = {
-      timeout = 1000,
-    } },
+    -- { 'rcarriga/nvim-notify', enabled = true, opts = {
+    --   timeout = 1000,
+    -- } },
   },
   require('noice').setup {
     routes = {
