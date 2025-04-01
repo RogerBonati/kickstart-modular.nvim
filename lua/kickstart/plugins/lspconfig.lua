19022e8e4b0a6de3545df90dc9d0c8572facacdc
Roger Bonati <roger.bonati@web.de>
2025-04-01 23:59:42 +0200

    configuring blink cmp in style of mr jacob

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -60,7 +60,8 @@ return {
       --    That is to say, every time a new file is opened that is associated with
       --    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
       --    function will be executed to configure the current buffer
-      local capabilities = require('blink.cmp').get_lsp_capabilities()
+      local original_capabilities = vim.lsp.buf_notifyotocol.make_client_capabilities()
+      local capabilities = require('blink.cmp').get_lsp_capabilities(original_capabilities)
       require('lspconfig').lua_ls.setup { capabilities = capabilities }
 
       vim.api.nvim_create_autocmd('LspAttach', {
