71fd3834bb5759f10dc664934da2aa81d0a7b8c1
Roger Bonati <roger.bonati@1und1.de>
2025-04-24 09:46:48 +0200

    comment diagnostics out again

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -8,8 +8,8 @@ end, { desc = 'conform test' })
 --
 -- Diagnostic keymaps
 
-vim.keymap.set('n', '[d', vim.diagnostic.jump { count = 1 }, { desc = 'Go to previous [D]iagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.jump { count = -1 }, { desc = 'Go to next [D]iagnostic message' })
+-- vim.keymap.set('n', '[d', vim.diagnostic.jump { count = 1 }, { desc = 'Go to previous [D]iagnostic message' })
+-- vim.keymap.set('n', ']d', vim.diagnostic.jump { count = -1 }, { desc = 'Go to next [D]iagnostic message' })
 
 -- vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 -- vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, {0, {scope="line"}, desc = 'Show diagnostic [E]rror messages' })
