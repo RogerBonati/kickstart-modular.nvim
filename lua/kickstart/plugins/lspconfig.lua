12c2c075aa70d2616bc66a4beb53e5c21b99a8b2
Roger Bonati <roger.bonati@web.de>
2025-08-06 14:42:55 +0200

    configured blink plugin for completion

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -62,6 +62,20 @@ return {
       local original_capabilities = vim.lsp.protocol.make_client_capabilities()
       local capabilities = require('blink.cmp').get_lsp_capabilities(original_capabilities)
 
+      require('blink.cmp').setup {
+        signature = { enabled = true },
+        completion = {
+          documentation = { auto_show = true, auto_show_delay_ms = 500 },
+          menu = {
+            auto_show = true,
+            draw = {
+              treesitter = { 'lsp' },
+              columns = { { 'kind_icon', 'label', 'label_description', gap = 1 }, { 'kind' } },
+            },
+          },
+        },
+      }
+
       require('lspconfig').lua_ls.setup { capabilities = capabilities }
 
       vim.api.nvim_create_autocmd('LspAttach', {
