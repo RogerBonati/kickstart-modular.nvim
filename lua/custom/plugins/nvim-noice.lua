5d6e9ff2cea4a157b971611fd2e13a693ad30fe3
bonati <roger.bonati@web.de>
2026-02-11 17:16:30 +0100

    corrected nuice plugin

--- lua/custom/plugins/nvim-noice.lua
+++ lua/custom/plugins/nvim-noice.lua
@@ -1,31 +1,15 @@
 return {
   'folke/noice.nvim',
   event = 'VeryLazy',
-  opts = {
-
-    -- routes = {
-    --   {
-    --     view = 'notify',
-    --     filter = { event = 'msg_showmode' },
-    --   },
-    -- },
-    -- add any options here
-    -- throttle = 1000 / 120, -- Adjust to your preference
-    throttle = 1000 / 480, -- Adjust to your preference
-  },
   dependencies = {
-    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
     'MunifTanjim/nui.nvim',
     'nvim-lualine/lualine.nvim',
-    -- OPTIONAL:
-    --   `nvim-notify` is only needed, if you want to use the notification view.
-    --   If not available, we use `mini` as the fallback
-    -- { 'rcarriga/nvim-notify', enabled = false },
-    -- { 'rcarriga/nvim-notify', enabled = true, opts = {
-    --   timeout = 1000,
-    -- } },
+    -- "rcarriga/nvim-notify", -- optional
   },
-  require('noice').setup {
+
+  opts = {
+    throttle = 1000 / 480,
+
     routes = {
       {
         view = 'notify',
@@ -35,29 +19,14 @@ return {
 
     views = {
       cmdline_popup = {
-        position = {
-          row = 5,
-          col = '50%',
-        },
-        size = {
-          width = 60,
-          height = 'auto',
-        },
+        position = { row = 5, col = '50%' },
+        size = { width = 60, height = 'auto' },
       },
       popupmenu = {
         relative = 'editor',
-        position = {
-          row = 8,
-          col = '50%',
-        },
-        size = {
-          width = 60,
-          height = 10,
-        },
-        border = {
-          style = 'rounded',
-          padding = { 0, 1 },
-        },
+        position = { row = 8, col = '50%' },
+        size = { width = 60, height = 10 },
+        border = { style = 'rounded', padding = { 0, 1 } },
         win_options = {
           winhighlight = { Normal = 'Normal', FloatBorder = 'DiagnosticInfo' },
         },
@@ -65,33 +34,30 @@ return {
     },
 
     lsp = {
-      -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
       override = {
         ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
         ['vim.lsp.util.stylize_markdown'] = true,
-        ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
+        ['cmp.entry.get_documentation'] = true,
       },
     },
-    -- you can enable a preset for easier configuration
+
     presets = {
-      bottom_search = true, -- use a classic bottom cmdline for search
-      command_palette = true, -- position the cmdline and popupmenu together
-      long_message_to_split = true, -- long messages will be sent to a split
-      inc_rename = false, -- enables an input dialog for inc-rename.nvim
-      lsp_doc_border = true, -- add a border to hover docs and signature help
+      bottom_search = true,
+      command_palette = true,
+      long_message_to_split = true,
+      inc_rename = false,
+      lsp_doc_border = true,
+    },
+
+    popupmenu = {
+      backend = 'leap',
+      kind_icons = false,
+    },
+
+    cmdline = {
+      format = {
+        popupmenu = { view = 'popup' },
+      },
     },
   },
-
-  -- test
-  popupmenu = {
-    backend = 'leap',
-    kind_icons = false,
-  },
-  cmdline = {
-    format = {
-      popupmenu = { view = 'popup' },
-    },
-  },
-
-  -- test
 }
