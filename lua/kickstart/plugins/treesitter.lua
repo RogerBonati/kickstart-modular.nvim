165a9a6632c2dadfe920839a7549bfa70744291a
bonati <roger.bonati@web.de>
2026-02-18 17:05:32 +0100

    suggestions from copilot

--- lua/kickstart/plugins/treesitter.lua
+++ lua/kickstart/plugins/treesitter.lua
@@ -22,6 +22,19 @@ return {
         'yaml',
       },
 
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true,
+          keymaps = {
+            ['af'] = '@function.outer',
+            ['if'] = '@function.inner',
+            ['ac'] = '@class.outer',
+            ['ic'] = '@class.inner',
+          },
+        },
+      },
+
       auto_install = true,
 
       highlight = {
