d2a35a287710bea20764d0c90ab4b46ee02a8ef4
bonati <roger.bonati@web.de>
2026-02-18 16:57:45 +0100

    correct leap

--- lua/custom/plugins/nvim-leap.lua
+++ lua/custom/plugins/nvim-leap.lua
@@ -1,15 +1,50 @@
+-- return {
+--   'ggandor/leap.nvim',
+--   -- init = function()
+--   -- 	require("leap").add_default_mappings()
+--   -- end,
+--   dependencies = {
+--     'tpope/vim-repeat',
+--   },
+--
+--   opts = {
+--     add_default_mappings = true,
+--   },
+--
+--   lazy = false,
+-- }
+
+-- return {
+--   {
+--     url = 'https://codeberg.org/andyg/leap.nvim',
+--     dependencies = {
+--       'tpope/vim-repeat',
+--     },
+--     config = function()
+--       require('leap').add_default_mappings()
+--     end,
+--   },
+-- }
+
 return {
-  'ggandor/leap.nvim',
-  -- init = function()
-  -- 	require("leap").add_default_mappings()
-  -- end,
-  dependencies = {
-    'tpope/vim-repeat',
-  },
+  {
+    url = 'https://codeberg.org/andyg/leap.nvim',
+    dependencies = { 'tpope/vim-repeat' },
 
-  opts = {
-    add_default_mappings = true,
-  },
+    build = false,
+    dev = false,
+    -- force full clone:
 
-  lazy = false,
+    config = function()
+      local leap = require 'leap'
+
+      -- These are the new recommended mappings
+      leap.add_repeat_mappings('s', '<enter>')
+
+      -- Standard motions
+      vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap-forward)')
+      vim.keymap.set({ 'n', 'x', 'o' }, 'S', '<Plug>(leap-backward)')
+      vim.keymap.set({ 'n', 'x', 'o' }, 'gs', '<Plug>(leap-from-window)')
+    end,
+  },
 }
